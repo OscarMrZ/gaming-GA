166ceab344ddf1c3cf128d0453d46dbf31d59648
Óscar Martínez <oscar.personal@tutanota.com>
2023-12-19 17:46:11 +0100

    First commit, moving the code

---
+++ game_v2.py
@@ -0,0 +1,330 @@
+import pygame
+import random
+import numpy as np
+import cv2
+import time
+
+import tensorflow as tf
+from tensorflow import keras
+
+NONE = 0
+JUMP = 1
+FALL = 2
+
+GROUND = 0
+AEREAL = 1
+
+INC_INTERVAL = 10000
+
+game_size = (400, 400)
+speed = 5
+max_speed = 20
+max_obs_margin = 400
+obs_size = (35, 35)
+
+### GAME CLASSES ###
+class Player:
+    def __init__(self, x, y, image_path, size, max_jump_height, brain):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, size)  # Resize the image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+        self.initial_y = y  # Save the initial y position
+        self.brain = brain
+        self.is_jumping = False
+        self.is_falling_fast = False
+        self.max_jump_height = max_jump_height
+        self.jump_speed = self.calculate_jump_speed()
+        self.fall_speed_increment = 3  # Base value for how fast the player falls
+        self.is_alive = True
+        self.score = 0
+        self.n_jumps = 0
+        self.train_frame = None
+
+    def calculate_jump_speed(self):
+        # Calculate the initial jump speed based on the maximum jump height
+        return (2 * self.max_jump_height) ** 0.5
+
+    def jump(self):
+        self.n_jumps += 1
+        if not self.is_jumping:
+            self.is_jumping = True
+            self.is_falling_fast = False  # Reset fast falling state when jumping
+            self.current_jump_speed = self.jump_speed
+            self.fall_speed_increment = 1  # Reset to normal fall speed
+
+    def fast_fall(self):
+        # Start fast falling
+        self.is_falling_fast = True
+
+    def move(self):
+
+        if self.is_jumping:
+            # Update the y position based on the current jump speed
+            self.rect.y -= self.current_jump_speed
+            # Adjust the gravity effect
+            gravity_effect = self.fall_speed_increment if not self.is_falling_fast else 6
+            self.current_jump_speed -= gravity_effect
+
+            # Check if the player has reached the ground
+            if self.rect.y > self.initial_y:
+                self.rect.y = self.initial_y
+                self.is_jumping = False
+                self.is_falling_fast = False  # Reset fast falling state when reaching ground
+                self.fall_speed_increment = 1  # Reset fall speed increment
+
+        # Ensure the player never goes below the initial y position
+        if self.rect.y > self.initial_y:
+            self.rect.y = self.initial_y
+            self.is_jumping = False
+            self.is_falling_fast = False  # Reset fast falling state
+            self.fall_speed_increment = 1  # Reset fall speed increment
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_path, size, speed, type):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, size)  # Resize the image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+        self.speed = speed
+        self.type = type
+
+    def move(self):
+        self.rect.x -= self.speed
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_path, width, height):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+
+### AUXILIARY CLASSES ###
+        
+def get_prepared_frame(game_size, frame_size, background, obstacles, player):
+    # Create a new surface
+    synthetic_surface = pygame.Surface(game_size)
+
+    # Render the background
+    synthetic_surface.blit(background.image, background.rect)
+
+    # Render obstacles and player
+    for obstacle in obstacles:
+        synthetic_surface.blit(obstacle.image, obstacle.rect)
+    synthetic_surface.blit(player.image, player.rect)
+
+    # Convert surface to an array and process it
+    frame = pygame.surfarray.array3d(synthetic_surface)
+    frame = np.transpose(frame, (1, 0, 2))  # Correct the orientation
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert to BGR
+
+    # The rest of the processing remains the same
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    resized_frame = cv2.resize(gray_frame, frame_size)
+    normalized_frame = resized_frame / 255.0
+
+    expanded_frame = np.expand_dims(normalized_frame, axis=-1)  # Add channel dimension
+    expanded_frame = np.expand_dims(expanded_frame, axis=0)     # Add batch dimension
+
+    return expanded_frame
+
+def count_types(obstacles, specific_type):
+    count = 0
+    for obstacle in obstacles:
+        if obstacle.type == specific_type:
+            count += 1
+    return count
+
+### CONTROL CLASSES ###
+
+def get_action(frame, model, print_probs = False):
+
+    # Make the prediction
+    probs = model.predict(frame, verbose=0)
+
+    # Convert the prediction to an action (we can choose arbitarely which class is each action)
+    max_index = np.argmax(probs[0])
+
+    if print_probs:
+        print(probs)
+    
+    return max_index
+
+### MAIN LOOP ###
+
+def run_game(sleep_time, model_list):
+
+    global speed
+
+    # Initialize the game
+    pygame.init()
+    start_time = pygame.time.get_ticks()  # Get the start time
+    win_width, win_height = game_size
+    ground_height = win_height - 75
+    win = pygame.display.set_mode((win_width, win_height))
+
+    # Create players, background, and ground instances
+    player_list = []
+    for model in model_list:
+        player = Player(50, ground_height - 15, './data/grinch.png', (50, 50), 150, model)
+
+        # Create a list of players (they have an initial score)
+        player_list.append(player)
+
+    background = Background('./data/background.png', win_width, win_height)
+    font = pygame.font.SysFont('arial', 18)  # Font name and size
+
+    obstacles = []
+    run = True
+    alive_players = len(player_list)
+
+    while run:
+
+        rng = random.randint(1, 100)
+
+        # Update the speed
+        for player in player_list:
+            if player.is_alive:
+                if speed < max_speed:
+                    speed = 5 + int(player.score / 4)
+                break
+
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # Move player
+        for player in player_list:
+            if player.is_alive:
+                player.move()
+
+        # Create new ground obstacles
+        if rng in list(range(1, 5)):
+            
+            n_ground = count_types(obstacles, GROUND)
+
+            if n_ground > 0:
+
+                # Calculate the adaptive margin
+                obs_margin = speed * 40
+                if obs_margin > max_obs_margin: obs_margin = max_obs_margin
+                
+                # Find the last ground obstacle
+                last_ground_obstacle = None
+                for obs in reversed(obstacles):
+                    if obs.type == GROUND:
+                        last_ground_obstacle = obs
+                        break
+
+                if last_ground_obstacle != None and win_width - last_ground_obstacle.rect.x > obs_margin:
+                    new_obs1 = Obstacle(win_width, ground_height, './data/candy.png', obs_size, speed, GROUND)
+                    obstacles.append(new_obs1)
+            else:
+                new_obs1 = Obstacle(win_width, ground_height, './data/candy.png', obs_size, speed, GROUND)
+                obstacles.append(new_obs1)
+        
+        # Create new aereal obstacles
+        if rng in list(range(10, 12)):
+
+            n_aereal = count_types(obstacles, AEREAL)
+
+            if n_aereal > 0:
+
+                # Calculate the adaptive margin
+                obs_margin = speed * 40
+                if obs_margin > max_obs_margin: obs_margin = max_obs_margin
+                
+                # Find the last ground obstacle
+                last_aereal_obstacle = None
+                for obs in reversed(obstacles):
+                    if obs.type == AEREAL:
+                        last_aereal_obstacle = obs
+                        break
+
+                if last_aereal_obstacle != None and win_width - last_aereal_obstacle.rect.x > obs_margin:
+                    new_obs1 = Obstacle(win_width, ground_height - 150, './data/noel.png', obs_size, speed, AEREAL)
+                    obstacles.append(new_obs1)
+            else:
+                new_obs1 = Obstacle(win_width, ground_height - 150, './data/noel.png', obs_size, speed, AEREAL)
+                obstacles.append(new_obs1)
+
+        # Move and remove obstacles
+        for ob in obstacles[:]:
+            ob.move()
+            if ob.rect.x < -ob.rect.width:
+                obstacles.remove(ob)
+                for player in player_list:
+                    if player.is_alive:
+                        player.score += 1
+
+        # Collision detection
+        for ob in obstacles:
+            for player in player_list:
+                if player.is_alive and player.rect.colliderect(ob.rect):
+                    player.is_alive = False
+                    alive_players -= 1
+
+        # Drawing
+        win.blit(background.image, background.rect)
+        for player in player_list:
+            if player.is_alive:
+                win.blit(player.image, player.rect)
+        for ob in obstacles:
+            win.blit(ob.image, ob.rect)
+
+        # Display the number of alive players
+        text = "Alive Players: " + str(alive_players)
+        text_surface = font.render(text, True, (255, 255, 255))  # Text, anti-aliasing, color (RGB)
+        win.blit(text_surface, (250, 20))  # Position (X, Y) where the text is drawn
+        pygame.display.update()
+
+        # Display the speed
+        text = "Speed: " + str(speed)
+        text_surface = font.render(text, True, (255, 255, 255))  # Text, anti-aliasing, color (RGB)
+        win.blit(text_surface, (150, 20))  # Position (X, Y) where the text is drawn
+        pygame.display.update()
+
+        # Generate a game frame
+        for player in player_list:
+            prepared_frame = get_prepared_frame((400, 400), (50, 50), background, obstacles, player)
+            player.frame = prepared_frame
+
+        # Get the control action and execute it
+        for player in player_list:
+            if player.is_alive:
+                action = get_action(player.frame, player.brain, False)
+                if action == JUMP:
+                    player.jump()
+                elif action == FALL:
+                    player.fast_fall()
+
+        # Check exit condition
+        if alive_players == 0:
+            run = False
+        
+        # Check for maximum time of 2 minutes
+        current_time = pygame.time.get_ticks()
+        if current_time - start_time > 120000:  # 120000 milliseconds = 2 minutes
+            run = False
+
+        time.sleep(sleep_time)
+
+    pygame.quit()
+
+    # Generate a list of scores
+    results = []
+    for player in player_list:
+        results.append((player.score, player.n_jumps))
+
+    return results
+
+def main():
+    
+    model = keras.models.load_model('v1.keras')
+    run_game(0, [model])
+
+if __name__ == "__main__":
+    main()
---
+++ trainer.py
@@ -0,0 +1,226 @@
+import pygame
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import sys
+import argparse
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, BatchNormalization
+from tensorflow.keras.models import clone_model
+
+import pygad.kerasga
+
+from game_v4 import run_game
+
+model = None
+
+# Testing
+def tiny_cnn(input_shape):
+
+    model = Sequential()
+
+    # Convolutional layers
+    model.add(Conv2D(8, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(BatchNormalization())
+
+    # Flatten the output
+    model.add(Flatten())
+
+    # Fully connected layer
+    model.add(Dense(8, activation='relu'))
+
+    # Output layer with 2 neurons (for "jump" and "fall") and sigmoid activation
+    model.add(Dense(3, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', # Changed to binary_crossentropy
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+def cute_net(input_shape):
+
+    model = Sequential()
+
+    # Conv layers
+    model.add(Conv2D(4, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Conv2D(8, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(16, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+
+    # Flatten the output
+    model.add(Flatten())
+
+    # Fully connected layers
+    model.add(Dense(32, activation='relu'))
+
+    # Output layer with 3 neurons (for a 3-class classification) and softmax activation
+    model.add(Dense(3, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+# Works
+def stable_net(input_shape):
+    
+    model = Sequential()
+
+    # Convolutional layers
+    model.add(Conv2D(16, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+
+    # Flatten the output
+    model.add(Flatten())
+
+    # Fully connected layer
+    model.add(Dense(32, activation='relu'))
+
+    # Output layer with 2 neurons (for "jump" and "fall") and sigmoid activation
+    model.add(Dense(3, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', # Changed to binary_crossentropy
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+### TRAINING FUNCTIONS ### 
+def fitness_func(ga_instance, solutions, solutions_indices):
+
+    global model
+
+    model_list = []
+    batch_fitness = []
+
+    for solution in solutions:
+
+        new_model = clone_model(model)
+
+        # Set the weights with the current solution
+        model_weights_matrix = pygad.kerasga.model_weights_as_matrix(model=new_model,
+                                                                weights_vector=solution)
+
+        new_model.set_weights(weights=model_weights_matrix)
+
+        model_list.append(new_model)
+
+    results = run_game(0, model_list)
+
+    for score, n_jumps in results:
+        fitness = 0
+        if n_jumps != 0:
+            fitness = score * score / n_jumps
+        
+        batch_fitness.append(fitness)
+
+    print("Batch Fitness: " + str(batch_fitness))
+    return batch_fitness
+
+def on_generation(ga_instance):
+    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution(ga_instance.last_generation_fitness)[1]), end='\n\n')
+
+## MAIN
+def main(pretrained_model_path=None):
+
+    global model
+
+    # Load an existing model or create a new one
+    if pretrained_model_path:
+        model = keras.models.load_model(pretrained_model_path)
+    else:
+        model = stable_net((75, 75, 4))
+        # model = cute_net((50, 50, 4))
+
+    model.summary()
+
+    try:
+
+        # Create the KERAS GA object
+        keras_ga = pygad.kerasga.KerasGA(model=model, num_solutions=15)
+
+        # Check if a pre-trained model is used
+        if pretrained_model_path:
+
+            # Get the current model weights
+            current_weights = model.get_weights()
+
+            # Convert the weights to a single flattened vector
+            current_weights_vector = np.concatenate([weight.flatten() for weight in current_weights])
+
+            # Replicate the weights vector for the entire initial population
+            initial_population = np.array([current_weights_vector for _ in range(keras_ga.num_solutions)])
+        else:
+            # Use default initialization
+            initial_population = keras_ga.population_weights
+
+        # Trainer object
+        ga_instance = pygad.GA(num_generations=250,
+                            num_parents_mating=6,
+                            fitness_func=fitness_func,
+                            fitness_batch_size=15,
+                            initial_population=initial_population,
+                            mutation_probability=0.20,
+                            on_generation=on_generation,
+                            suppress_warnings=True)
+
+        # Running the genetic algorithm
+        ga_instance.run()
+
+        # Plotting the training process
+        fig = ga_instance.plot_fitness(title="PyGAD & Keras - Iteration vs. Fitness", linewidth=4)
+        fig.savefig("GA_Training_Process.png")  # Save the plot to a file
+
+    except KeyboardInterrupt:
+
+        print("\nCtrl+C pressed. Saving the current best model...")
+
+        # Plotting the training process
+        fig = ga_instance.plot_fitness(title="PyGAD & Keras - Iteration vs. Fitness", linewidth=4)
+        fig.savefig("GA_Training_Process.png")  # Save the plot to a file
+
+        # Retrieve and save the current best solution
+        solution, solution_fitness, _ = ga_instance.best_solution()
+        best_solution_weights = pygad.kerasga.model_weights_as_matrix(model=model, weights_vector=solution)
+        model.set_weights(weights=best_solution_weights)
+        model.save("best_model_interrupted.keras")
+
+        print("Best model saved as 'best_model_interrupted.keras'. Exiting.")
+        sys.exit(0)
+
+    # Retrieving and saving the final best solution
+    solution, solution_fitness, _ = ga_instance.best_solution()
+    best_solution_weights = pygad.kerasga.model_weights_as_matrix(model=model, weights_vector=solution)
+    model.set_weights(weights=best_solution_weights)
+    model.save("best_model.keras")
+
+    # Print information about the best solution
+    print("Parameters of the best solution:\n{solution}".format(solution=solution), end="\n\n")
+    print("Length of the solution is:", len(solution), end='\n\n')
+    print("Fitness value of the best solution:\n{solution_fitness}".format(solution_fitness=solution_fitness), end='\n\n')
+
+if __name__ == "__main__":
+
+    # Create argument parser
+    parser = argparse.ArgumentParser(description='Run the training process with an optional pre-trained model.')
+
+    # Add argument for the pre-trained model path
+    parser.add_argument('--model_path', type=str, default=None, help='Path to a pre-trained model. If not provided, a new model will be created.')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Run the main function with the provided model path
+    main(pretrained_model_path=args.model_path)
